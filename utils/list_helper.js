d6584f7b916893c5f71ed94fa3b9bb762988cc2b
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-16 11:33:29 +0200

    Add jest tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sumOfLikes = blogs
+    .reduce((acc, cur) => acc + cur.likes, 0)
+  
+  return sumOfLikes
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
