9a956b9396acce401e8d201e795b0dffda4774e1
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-16 17:22:07 +0200

    First functional version of 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,7 @@ const dummy = () => {
 const totalLikes = (blogs) => {
   const sumOfLikes = blogs
     .reduce((acc, cur) => acc + cur.likes, 0)
-  
+
   return sumOfLikes
 }
 
@@ -25,6 +25,37 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  const countedAuthors = blogs
+    .reduce((allAuthors, currentBlog) => {
+      if (currentBlog.author in allAuthors) {
+        allAuthors[currentBlog.author]++
+      } else {
+        allAuthors[currentBlog.author] = 1
+      }
+      return allAuthors
+    }, {})
+  
+  console.log('countedBlogs', countedAuthors)
+  const maxCount = Math.max(...Object.values(countedAuthors))
+
+  const authorArray = Object.entries(countedAuthors)
+
+  const mostProductiveAuthor = authorArray.filter(author => author[1] === maxCount)
+
+  console.log('last: ', mostProductiveAuthor)
+  
+  return (
+    {
+      author: mostProductiveAuthor[0][0],
+      blogs: mostProductiveAuthor[0][1]
+    }
+  )
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
