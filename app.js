6d42425d83fad5f433394448afebce2c3f8db199
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-23 10:01:03 +0200

    Add login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.set('useFindAndModify', false)
@@ -18,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
